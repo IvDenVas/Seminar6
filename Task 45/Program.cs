2e79d7f5ccd68c8d58dd48dc473609bb8a689603
Denis <ivdenvas@gmail.com>
2022-11-30 15:52:18 +0300

    finish2

--- Task 45/Program.cs
+++ Task 45/Program.cs
@@ -12,7 +12,7 @@ int[] ReversArray1(int[] inArray)
     int[] result = new int[inArray.Length];
     for (int i = 0; i < inArray.Length; i++)
     {
-        result[i] = inArray[inArray.Length - 1 - i];
+        result[i] = inArray[i];
     }
     return result;
 }
