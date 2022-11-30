07356b33dbb1f35f64774768327f04b1b094f0a8
Denis <ivdenvas@gmail.com>
2022-11-30 14:25:55 +0300

    finish

---
+++ Task 40/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с стороноами такой длины.
+
+int num1 = userInput("Введите первое число: ");
+int num2 = userInput("Введите второе число: ");
+int num3 = userInput("Введите третье число: ");
+
+if (Triangle(num1, num2, num3))
+    Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
+static int userInput (string message)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if(int.TryParse(Console.ReadLine(), out int num))
+            if (num > 0) return num;
+        Console.WriteLine("Ошибка ввода!");
+    }
+}
+
+static bool Triangle(int num1, int num2, int num3)
+{
+    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) return true; // return (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2)
+    else return false;                                                               //
+}
